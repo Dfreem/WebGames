71343cd95dcd1d720ca5097bc119ff48e728e41e
Dfreem <freemand@my.lanecc.edu>
2024-06-19 21:46:57 -0700

    menu modal

--- WebGames/wwwroot/app.js
+++ WebGames/wwwroot/app.js
@@ -1,24 +1,7 @@
 ﻿
 export function initKeyboardListener(keyboardListener) {
-    console.log('registering keyboard listener');
     window.addEventListener('keydown', (e) => {
-<<<<<<< Updated upstream
-        console.log(e);
-        if (e.key === 'ArrowLeft') {
-            keyboardListener.invokeMethodAsync('Left');
-        }
-        if (e.key === 'ArrowRight') {
-            keyboardListener.invokeMethodAsync('Right');
-        }
-        if (e.key === 'ArrowDown') {
-            keyboardListener.invokeMethodAsync('Down');
-        }
-        if (e.key === 'ArrowUp') {
-            keyboardListener.invokeMethodAsync('Up');
-        }
-=======
         keyboardListener.invokeMethodAsync('GetKey', e.key);
->>>>>>> Stashed changes
-    })
+    });
 
 }
