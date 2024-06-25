065383bf9b9b9c1e28e80930754cd641c443bb5e
Dfreem <freemand@my.lanecc.edu>
2024-06-24 18:11:46 -0700

    --wip-- [skip ci]

--- WebGames/Program.cs
+++ WebGames/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using WebGames;
+using WebGames.Shared.Components.Menu;
 using WebGames.Shared.Components.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IToastService, ToastService>();
+builder.Services.AddTransient<MenuJSInteropt>();
 
 
 await builder.Build().RunAsync();
---
+++ WebGames/Shared/Components/Menu/MenuJSInteropt.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace WebGames.Shared.Components.Menu;
+
+public class MenuJSInteropt(IJSRuntime _js)
+{
+    public IJSObjectReference Module { get; set; } = default!;
+
+    public async Task ToggleModalAsync(string menuId)
+    {
+        Module = await _js.InvokeAsync<IJSObjectReference>("import", "/Shared/Components/Menu/Menu.razor.js");
+        await Module.InvokeVoidAsync("toggleModal", menuId);
+    }
+    public async Task ShowModalAsync(string menuId)
+    {
+        Module = await _js.InvokeAsync<IJSObjectReference>("import", "/Shared/Components/Menu/Menu.razor.js");
+        await Module.InvokeVoidAsync("showModal", menuId);
+    }
+    public async Task HideModalAsync(string menuId)
+    {
+        Module = await _js.InvokeAsync<IJSObjectReference>("import", "/Shared/Components/Menu/Menu.razor.js");
+        await Module.InvokeVoidAsync("hideModal", menuId);
+    }
+}
